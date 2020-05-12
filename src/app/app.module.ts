70e6f450cd3be8edba20e2eed3e6c29e37ad46f3
Sean Blessing <snblessing@gmail.com>
2020-05-12 10:40:56 -0400

    movie, actor, credit list functions complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieListComponent } from './feature/movie/movie-list/movie-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ActorListComponent } from './feature/actor/actor-list/actor-list.component';
+import { CreditListComponent } from './feature/credit/credit-list/credit-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieListComponent
+    MovieListComponent,
+    ActorListComponent,
+    CreditListComponent
   ],
   imports: [
     BrowserModule,
